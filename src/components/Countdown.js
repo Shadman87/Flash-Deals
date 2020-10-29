451431e17f66c315437094bbe6352eee0d8ffa60
Shadman <shimantoh83@gmail.com>
2020-10-29 12:13:47 +0600

    Edit on click and card hover added

---
+++ src/components/Countdown.js
@@ -0,0 +1,54 @@
+import React from 'react'; 
+
+class Countdown extends React.Component {
+  state = {
+    days: undefined, 
+    hours: undefined, 
+    minutes: undefined, 
+    seconds: undefined, 
+  }; 
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const date = this.props.date; 
+      const time = this.props.time;
+      const countDownDate = new Date(date + " " + time).getTime();
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now;
+    
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+     
+
+      this.setState({ days, hours, minutes, seconds }); 
+    }, 1000); 
+  }
+
+  componentWillUnmount() {
+    if(this.interval) {
+      clearInterval(this.interval); 
+    }
+  }
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state; 
+
+    return (
+      <div>
+        <div className="countdown-wrapper">
+          <div className="countdown-item">
+            {days} : {hours} : {minutes} : {seconds}
+          </div>
+          
+        </div>
+      </div>
+    ); 
+  }
+}
+
+export default Countdown; 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from "react"; 
 import { Link } from 'react-router-dom';
+
+//Importing Components
 import Navbar from './Navbar';
+import Countdown from './Countdown';
 
 import "../App.css"; 
 
+//Importing functions with api
 import firestoreGetData from "../api/firestoreGetData";
 import firestoreDeleteData from "../api/firestoreDeleteData";
 import authListener from "../api/authListener"; 
@@ -30,21 +34,34 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      
       <div className="container">
+      
         <div className="row ">
-        {deals.map(doc => (
-          <div key={doc.id} className="col mt-5">
-            <div className="card">  
-              <div className="content">
-                <img src={doc.data().imageUrl} className="card-img-top" alt=""/>
-                <div className="text-block">
-                  <h5>{doc.data().title}</h5>
+          {deals.map(doc => (
+            <div key={doc.id} className="col-md-4 mt-5">
+              <Link to={`/deals/edit/${doc.id}`}>
+                <div className="card">  
+                  <div className="content">
+                    <img src={doc.data().imageUrl} className="card-img-top" alt=""/>
+                    <div className="title-block">
+                      <h6>{doc.data().title}</h6>
+                    </div>
+                    <div className="deletBtn-block">
+                      <i className="fa fa-trash"/>
+                    </div>
+                    <div className="message-block">
+                      <p>{doc.data().message}</p>
+                    </div>
+                    <div className="timer-block">
+                      <Countdown date={doc.data().date} time={doc.data().time} />
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </div>
   ); 
